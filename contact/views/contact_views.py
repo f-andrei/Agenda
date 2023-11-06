174b9b4f77b96684c252b11fddabc9a848bec311
f-andrei <andreifrancap@gmail.com>
2023-11-06 16:12:48 -0300

    add pagination

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.db.models import Q
-from django.http import Http404
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
+    contacts = Contact.objects \
+        .filter(show=True) \
+        .order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     print(contacts.query)
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -34,12 +40,16 @@ def search(request):
             Q(phone__icontains=search_value) |
             Q(email__icontains=search_value) 
         ) \
-        .order_by('-id') \
-        
+        .order_by('-id')
     
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+        
     print(contacts.query)
+    
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - '
     }
 
@@ -50,7 +60,6 @@ def search(request):
     )
 
 def contact(request, contact_id):
-#   single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
     contact_name = f'{single_contact.first_name} {single_contact.last_name}'
