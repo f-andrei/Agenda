fd704c72d9aceb038ca58ee8c597d8a7a55f5d1c
f-andrei <andreifrancap@gmail.com>
2023-11-08 15:42:39 -0300

    add login

--- contact/urls.py
+++ contact/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     
     # user 
     path('user/create/', views.register, name='register'),
-    #path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
-    #path('contact/<int:contact_id>/update', views.update, name='update'),
-    #path('contact/<int:contact_id>/delete', views.delete, name='delete'),
+    path('user/login/', views.login_view, name='login'),
+    path('user/logout/', views.logout_view, name='logout'),
+    
 ]
--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,6 +1,9 @@
 from django.shortcuts import redirect, render
 from contact.forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+
 
 def register(request):
     form = RegisterForm()
@@ -21,3 +24,30 @@ def register(request):
             'form': form
         }
     )
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'Logado com sucesso!')
+            
+            return redirect('contact:index')
+
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form
+        }
+    )
+
+
+def logout_view(request):
+    auth.logout(request)
+    
+    return redirect('contact:login')
